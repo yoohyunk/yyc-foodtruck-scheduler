060123626ccd37079a5ebc86f1a81da3265976d8
yoohyunk <yoohyunk20@gmail.com>
2025-05-27 18:12:39 -0600

    chore(types): extract employee types into a separate file

---
+++ app/types.ts
@@ -0,0 +1,22 @@
+export interface Employee {
+  id: number;
+  name: string;
+  address: string;
+  role: string;
+  email: string;
+  phone: string;
+  wage: string;
+  isAvailable: boolean;
+  availability: string[];
+}
+
+export interface FormData {
+  name: string;
+  address: string;
+  role: string;
+  email: string;
+  phone: string;
+  wage: string;
+  isAvailable: boolean;
+  availability: string[];
+}
