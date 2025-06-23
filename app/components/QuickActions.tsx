65d4f8bf01138c8f7296f7e7341e85c03d5062a7
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-06-23 15:06:01 -0600

    removed duplicate buttons from header and made it show if youre on one of the linked pages quick actions also moved to hamburger menu when size is too small

--- app/components/QuickActions.tsx
+++ app/components/QuickActions.tsx
@@ -16,7 +16,7 @@ export default function QuickActions() {
   return (
     <TutorialHighlight
       isHighlighted={shouldHighlight(".sidebar")}
-      className="sidebar"
+      className="sidebar hidden xl:block"
     >
       <aside className="sidebar bg-gray-100 p-3 shadow-md">
         <h3 className="text-md font-semibold mb-6">Quick Actions</h3>
