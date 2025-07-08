62d66f140715a617690a07070c0397af01336f0c
yoohyunk <yoohyunk20@gmail.com>
2025-07-07 23:03:18 -0600

    update types with new type gen

--- app/types.ts
+++ app/types.ts
@@ -9,6 +9,7 @@ export type Employee = Tables<"employees"> & {
   addresses?: Tables<"addresses">;
   currentWage?: number;
 };
+export type EmployeeAvailability = Tables<"employee_availability">;
 export type Truck = Tables<"trucks">;
 export type TruckAssignment = Tables<"truck_assignment">;
 export type TruckAssignmentCreate = {
